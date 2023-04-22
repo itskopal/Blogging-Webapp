4addac7091d650fcc4ef11641993ee205e3df72f
itskopal <itskopal021@gmail.com>
2023-04-22 22:19:08 +0530

    minor changes

--- public/js/home.js
+++ public/js/home.js
@@ -21,6 +21,16 @@ const createBlog = (blog) => {
 }
 
 
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)  
+
+
 //active link
 $(document).ready(function(){
     // Add active to btn
